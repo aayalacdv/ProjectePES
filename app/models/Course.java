08d88824b4db58190bc3d2a234c2ebf0933b8012
aayalacdv <aayalacdv2000@gmail.com>
2021-01-04 22:49:06 +0100

    added functions to manage marks

--- app/models/Course.java
+++ app/models/Course.java
@@ -12,9 +12,10 @@ import play.db.jpa.Model;
 @Entity
 public class Course extends Model {
     public String name; 
-    public int finalGrade; 
-    public int currentGrade; 
-    public int meanToPass; 
+    public double finalGrade; 
+    public double currentGrade; 
+    public double meanToPass; 
+    public int percentageLeft;
 
 
     @ManyToOne
@@ -26,10 +27,45 @@ public class Course extends Model {
 
     public Course (String name, User user){
         this.name = name;
-        this.currentGrade = 0; 
         this.finalGrade = 0; 
         this.currentGrade = 0; 
+        this.percentageLeft = 100; 
+        
         this.user = user; 
         this.exams = new ArrayList<Exam>(); 
     }
+
+
+    public Course addExam ( Exam exam){
+        this.exams.add(exam); 
+        this.save(); 
+        return this; 
+    }
+
+
+    public Course updateParammeters (Exam exam){
+       if (this.percentageLeft >= exam.percentage ){
+            this.currentGrade =  this.currentGrade + exam.score * exam.percentage / 100;
+            this.percentageLeft = this.percentageLeft - exam.percentage; 
+            if(this.currentGrade < 5 && this.percentageLeft != 0){
+                this.meanToPass = (5 - this.currentGrade)*100.0/this.percentageLeft ;
+                this.meanToPass = Math.round(this.meanToPass * 100.0)/100.0;  
+            }
+            else {
+                this.meanToPass = 0; 
+            }
+            if (this.percentageLeft == 0){
+                this.finalGrade = this.currentGrade; 
+                this.meanToPass = 0; 
+            }
+       }
+       else {
+           return this; 
+       }
+        this.save();
+        return this; 
+    }
+
+
+    
 }
--- app/models/Exam.java
+++ app/models/Exam.java
@@ -8,7 +8,7 @@ import play.db.jpa.Model;
 @Entity
 public class Exam extends Model{
    public String name;
-   public int score; 
+   public double score; 
    public int percentage;  
 
 
@@ -16,11 +16,18 @@ public class Exam extends Model{
    public Course course;  
 
 
-   public Exam (String name, Course course, int percentage ){
+   public Exam(String name, Course course, int percentage ){
        this.name = name; 
        this.course = course; 
        this.percentage = percentage; 
        this.score = 0;  
        
+   }
+
+   public Exam addScore(double score){
+       this.score = score;
+       this.course.updateParammeters(this); 
+       this.save(); 
+       return this; 
    }
 }
